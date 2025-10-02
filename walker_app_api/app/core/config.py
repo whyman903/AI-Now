20e80b5a6e2b85c590c31b325fa664f8b6ae9c31
whyman903 <whyman903@gmail.com>
2025-10-02 13:36:10 -0400

    workflow for aggregation

--- walker_app_api/app/core/config.py
+++ walker_app_api/app/core/config.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "FeedCurator API"
+    PROJECT_NAME: str = "AI-Now API"
     VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
     PUBLIC_BASE_URL: Optional[str] = None
